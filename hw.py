34e0078e28d0d88197ef1cea8351c4e16b61c786
Smarty <mailkspriyanka@gmail.com>
2018-01-14 11:36:02 +0530

    Opens websites through python

--- hw.py
+++ hw.py
@@ -2,4 +2,4 @@ import webbrowser
 webbrowser.open_new_tab('https://www.facebook.com/')
 webbrowser.open_new_tab('https://www.youtube.com/')
 webbrowser.open_new_tab('https://google.com/')
-webbrowser.open_new_tab('https://gmail.com/')
+webbrowser.open_new_tab('https://www.pluralsight.com/')
