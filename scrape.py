3682b7130288f40cdd57cde0630067a7e1e002c0
Smarty <mailkspriyanka@gmail.com>
2018-01-14 11:28:59 +0530

    Fix issue by taking url as input

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 import requests
 a=[]
-url = 'http://www.insightsonindia.com/category/secure-synopsis/'
+url = input("Please enter the required url: ")
 r = requests.get(url)
 html_doc = r.text
 soup = BeautifulSoup(html_doc,"html.parser")
@@ -13,16 +13,3 @@ for head in heads:
 for link in a:
 	for address in link:
 		print(address.get('href'))
-for num in range(2,4):
-	b = []
-	urlx = 'http://www.insightsonindia.com/category/secure-synopsis/page/'+ str(num) +'/'
-	r = requests.get(urlx)
-	html_doc = r.text
-	soup = BeautifulSoup(html_doc,"html.parser")
-	heads = soup.find_all('h2',{"class":"post-title entry-title"})
-	for head in heads:
-		final = head.find_all('a',{'rel':'bookmark'})
-		b.append(final)
-	for link in b:
-		for address in link:
-			print(address.get('href'))
