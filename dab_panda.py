8f27b63606f25e146e4cafe4674baf31e046b37c
Smarty <mailkspriyanka@gmail.com>
2018-01-14 11:33:42 +0530

    Returns the url of the images by web scraping

--- dab_panda.py
+++ dab_panda.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 url = 'https://www.biographyonline.net/people/successful.html'
-r = requests.get(url)
-html_doc = r.text
+req = requests.get(url)
+html_doc = req.text
 soup = BeautifulSoup(html_doc)
 pics = soup.find_all('img')
 for link in pics:
