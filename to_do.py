200517195790c2a43e6dfd1678356fac1b845dbe
Smarty <mailkspriyanka@gmail.com>
2018-02-02 20:45:32 +0530

    Print indices in the 'children' list at which blockquotes occur

--- to_do.py
+++ to_do.py
@@ -1,11 +1,18 @@
 from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 import requests
+
 url = 'http://www.insightsonindia.com/2018/01/31/secure-synopsis-28-october-2017/'
 r = requests.get(url)
 html_doc = r.text
 soup = BeautifulSoup(html_doc,'html.parser')
 a = soup.find_all(class_="pf-content")
+bqs = soup('blockquote')
+kids = []
+count = 0
 for x in a :
-	for child in x.descendants:
-		print(type(child))
+	for item in x.descendants:
+		kids.append(item)
+for k in range(0,len(kids)):
+	if kids[k] in bqs:
+		print(k)
