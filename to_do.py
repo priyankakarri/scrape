95fc8ed6920a210a54018ba9056c1971abb8f888
Smarty <mailkspriyanka@gmail.com>
2018-02-12 16:40:22 +0530

    Concatenate elts btw two consecutive bqs and print

--- to_do.py
+++ to_do.py
@@ -2,18 +2,12 @@ from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 import requests
 
-url = 'http://www.insightsonindia.com/2018/01/31/secure-synopsis-28-october-2017/'
+url = 'http://www.insightsonindia.com/2017/11/21/secure-synopsis-07-october-2017/'
 r = requests.get(url)
 html_doc = r.text
 soup = BeautifulSoup(html_doc,'html.parser')
-a = soup.find_all(class_="pf-content")
-bqs = soup('blockquote')
-kids = []
-count = 0
-for x in a :
-	for child in x.descendants:
-		if child.name == 'blockquote':
-			topic = child.get_text()
-			print(topic)
-		else:
-			kids.append(child)
+a = soup.find_all(class_="pf-content")[0]
+answers = []
+for var in a.blockquote.next_siblings:
+	answers=var.get_text()
+	print(answers)
