2c2304e485d9360b0f559fa38b63d9fd7929b005
Smarty <mailkspriyanka@gmail.com>
2018-02-12 20:21:32 +0530

    Put the code in a Python method.

--- to_do.py
+++ to_do.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 import requests
-
-url = 'http://www.insightsonindia.com/2017/11/21/secure-synopsis-07-october-2017/'
-r = requests.get(url)
-html_doc = r.text
-soup = BeautifulSoup(html_doc,'html.parser')
-a = soup.find_all(class_="pf-content")[0]
-answers = []
-for var in a.blockquote.next_siblings:
-	answers=var.get_text()
-	print(answers)
+def func_name(url):
+	r = requests.get(url)
+	html_doc = r.text
+	soup = BeautifulSoup(html_doc,'html.parser')
+	a = soup.find_all(class_="pf-content")[0]
+	answers = []
+	for var in a.blockquote.next_siblings:
+		answers=var.get_text()
+		print(answers)
+	return answers
