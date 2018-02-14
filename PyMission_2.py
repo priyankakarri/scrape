943003df2ef02a74e9f0c1cbf39d0df62ad73051
Smarty <mailkspriyanka@gmail.com>
2018-02-14 20:04:16 +0530

    Check for three sample URLs

--- PyMission_2.py
+++ PyMission_2.py
@@ -1,12 +1,22 @@
 from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 import requests
-url = input('Enter any url to print the actors: ')
-r = requests.get(url)
-html_doc = r.text
-soup = BeautifulSoup(html_doc,'html.parser')
-actors = soup.find_all('span',{'itemprop': 'actors'})
-rating = soup.find_all('span',{'itemprop':'ratingValue'})[0]
-for actor in actors:
-	print(actor.get_text())
-print('Movie rating: ',rating.get_text())
+l = []
+for i in range(0,3):
+	url = input('Enter any url to print the actors: ')
+	r = requests.get(url)
+	html_doc = r.text
+	soup = BeautifulSoup(html_doc,'html.parser')
+	a = []
+	favs = ['\nAamir Khan,             ','\nAmitabh Bachchan,             ']
+	actors = soup.find_all('span',{'itemprop': 'actors'})
+	rating = soup.find_all('span',{'itemprop': 'ratingValue'})[0].get_text()
+	title = soup.find_all('h1',{'itemprop': 'name'})[0].get_text()
+	title = title.replace(u'\xa0',u' ')
+	for actor in actors:
+		a.append(actor.get_text())
+	for hero in a:
+		if hero in favs:
+			if (float(rating))>7.5 and (float(rating))<8.5:
+				l.append(title)
+print(l)
