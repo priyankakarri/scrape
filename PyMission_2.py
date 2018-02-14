45fe6047a30bd61ccca92cf9dc093eb0ae9d6477
Smarty <mailkspriyanka@gmail.com>
2018-02-14 16:04:12 +0530

    Print IMDb Rating

--- PyMission_2.py
+++ PyMission_2.py
@@ -6,5 +6,7 @@ r = requests.get(url)
 html_doc = r.text
 soup = BeautifulSoup(html_doc,'html.parser')
 actors = soup.find_all('span',{'itemprop': 'actors'})
+rating = soup.find_all('span',{'itemprop':'ratingValue'})[0]
 for actor in actors:
 	print(actor.get_text())
+print('Movie rating: ',rating.get_text())
