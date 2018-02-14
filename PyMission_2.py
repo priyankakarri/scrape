216be2799bab3b9ed2d34529b9b914dc03b9215c
Smarty <mailkspriyanka@gmail.com>
2018-02-14 22:23:58 +0530

    Fetch all movie urls and scrape

--- PyMission_2.py
+++ PyMission_2.py
@@ -1,22 +1,30 @@
 from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 import requests
-l = []
-for i in range(0,3):
-	url = input('Enter any url to print the actors: ')
-	r = requests.get(url)
-	html_doc = r.text
-	soup = BeautifulSoup(html_doc,'html.parser')
+links = []
+main_url = input("Enter the main url to get the other urls from: ")
+r1 = requests.get(main_url)
+html_doc = r1.text
+soup1 = BeautifulSoup(html_doc,'html.parser')
+urls = soup1.find_all('td',{'class':'titleColumn'})
+ratings = soup1.find_all('td',{'class':'ratingColumn imdbRating'})
+for url,rating in zip(urls,ratings):
+	for rating in ratings:
+		if (float(rating.get_text()))>7.5 and (float(rating.get_text()))<8.5:
+			links.append('http://www.imdb.com'+str(url.a['href']))
+l=[]
+for link in links:
+	r2 = requests.get(link)
+	html_docu = r2.text
+	soup2 = BeautifulSoup(html_docu,'html.parser')
 	a = []
 	favs = ['\nAamir Khan,             ','\nAmitabh Bachchan,             ']
-	actors = soup.find_all('span',{'itemprop': 'actors'})
-	rating = soup.find_all('span',{'itemprop': 'ratingValue'})[0].get_text()
-	title = soup.find_all('h1',{'itemprop': 'name'})[0].get_text()
+	actors = soup2.find_all('span',{'itemprop': 'actors'})
+	title = soup2.find_all('h1',{'itemprop': 'name'})[0].get_text()
 	title = title.replace(u'\xa0',u' ')
 	for actor in actors:
 		a.append(actor.get_text())
-	for hero in a:
-		if hero in favs:
-			if (float(rating))>7.5 and (float(rating))<8.5:
+	for fav in a:
+		if fav in favs:
 				l.append(title)
 print(l)
